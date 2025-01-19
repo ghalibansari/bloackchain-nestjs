b3139fe7fd07d00aa9de0970fd8e5fc2ca5491c8
Ghalib Ansari <ghalibansari1994@gmail.com>
2025-01-20 00:15:54 +0530

    Refactor CronRunner to improve initialization timing
    - Updated the timeout duration in CronRunner from 2000ms to 3000ms using a constant ONE_SECOND for better readability and maintainability.
    - Added a comment to clarify that the fetchPrice method runs once when the application starts.
    
    These changes enhance the clarity and functionality of the cron job initialization process.

--- src/cron/cron.runner.ts
+++ src/cron/cron.runner.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CronService } from './cron.service';
 import { Cron } from '@nestjs/schedule';
+import { ONE_SECOND } from 'src/app.constant';
 
 @Injectable()
 export class CronRunner {
   constructor(private readonly cronService: CronService) {
     setTimeout(async () => {
+      // run once when app start
       await this.fetchPrice();
-    }, 2000);
+    }, ONE_SECOND * 3);
   }
 
   @Cron('0 */5 * * * *')
